2e7a5932476c555e8d3997374748d7742b9129cd
Jesse Squires <jesse.d.squires@gmail.com>
2016-09-14 08:20:00 -0700

    Checkin more Pods/ resources.

---
+++ Pods/Target Support Files/OCMock/OCMock-umbrella.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+#import "OCMock.h"
+#import "OCMockObject.h"
+#import "OCMArg.h"
+#import "OCMConstraint.h"
+#import "OCMLocation.h"
+#import "OCMMacroState.h"
+#import "OCMRecorder.h"
+#import "OCMStubRecorder.h"
+#import "NSNotificationCenter+OCMAdditions.h"
+#import "OCMFunctions.h"
+
+FOUNDATION_EXPORT double OCMockVersionNumber;
+FOUNDATION_EXPORT const unsigned char OCMockVersionString[];
+
