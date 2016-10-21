43f574f70aa59e28361cb53e2557e114f1ccd90c
Ryan Nystrom <rnystrom@fb.com>
2016-10-21 09:47:16 +0800

    Working range example header copyright

--- Example/IGListKitExamples/ViewControllers/WorkingRangeViewController.swift
+++ Example/IGListKitExamples/ViewControllers/WorkingRangeViewController.swift
@@ -1,10 +1,16 @@
-//
-//  WorkingRangeViewController.swift
-//  IGListKitExamples
-//
-//  Created by Ryan Nystrom on 10/20/16.
-//  Copyright © 2016 Instagram. All rights reserved.
-//
+/**
+ Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
+
+ The examples provided by Facebook are for non-commercial testing and evaluation
+ purposes only. Facebook reserves all rights not expressly granted.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 import UIKit
 import IGListKit
