cc87d102ab71c820da32b6c50e09ad131f235f40
Sherlock <james@sherlo.uk>
2016-10-22 15:31:51 +0100

    Added Today/iMessage extension targets with basic examples

--- Example/Pods/Target Support Files/Pods-IGListKitTodayExample/Pods-IGListKitTodayExample-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-IGListKitTodayExample/Pods-IGListKitTodayExample-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/IGListKit-iOS8.0/IGListKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/IGListKit-iOS10.0/IGListKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/IGListKit-iOS8.0/IGListKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/IGListKit-iOS10.0/IGListKit.framework"
 fi
