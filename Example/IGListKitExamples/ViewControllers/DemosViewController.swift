94b92013ceace527e802120a65ae3fe50eaf162c
Ryan Nystrom <rnystrom@instagram.com>
2016-10-26 16:33:31 -0700

    Fix bad merge
    Summary: Not sure why the build badge is green. Also need to get this to block landing internally.
    
    Reviewed By: jessesquires
    
    Differential Revision: D4084875
    
    fbshipit-source-id: 67603f7db379f7311dc9c99d77c2e7ce08aff9b1

--- Example/IGListKitExamples/ViewControllers/DemosViewController.swift
+++ Example/IGListKitExamples/ViewControllers/DemosViewController.swift
@@ -31,7 +31,6 @@ class DemosViewController: UIViewController, IGListAdapterDataSource {
         DemoItem(name: "Single Section Controller", controllerClass: SingleSectionViewController.self),
         DemoItem(name: "Storyboard", controllerClass: SingleSectionViewController.self, controllerIdentifier: "demo"),
         DemoItem(name: "Single Section Storyboard", controllerClass: SingleSectionViewController.self, controllerIdentifier: "singleSectionDemo"),
-        DemoItem(name: "Working Range", controllerClass: WorkingRangeViewController.self)
         DemoItem(name: "Working Range", controllerClass: WorkingRangeViewController.self),
         DemoItem(name: "Diff Algorithm", controllerClass: DiffTableViewController.self),
     ]
