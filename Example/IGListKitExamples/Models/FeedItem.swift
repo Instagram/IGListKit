8fa4001d15720ece1de267c24bd420fbda498e81
Ryan Nystrom <rnystrom@instagram.com>
2016-11-07 06:37:37 -0800

    Add supplementary source example
    Summary:
    Got around to adding a supplementary view source example.
    
    Fixes #153 #163
    
    ![img_0197](https://cloud.githubusercontent.com/assets/739696/20042159/03d2489e-a443-11e6-8d60-895256b56273.PNG)
    
    - [x] All tests pass. Demo project builds and runs.
    - [x] I have reviewed the [contributing guide](https://github.com/Instagram/IGListKit/blob/master/CONTRIBUTING.md)
    Closes https://github.com/Instagram/IGListKit/pull/169
    
    Differential Revision: D4139910
    
    Pulled By: rnystrom
    
    fbshipit-source-id: 846c61c4d097392f18778d46044d4989e6bdf183

---
+++ Example/IGListKitExamples/Models/FeedItem.swift
@@ -0,0 +1,43 @@
+/**
+ Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
+
+ The examples provided by Facebook are for non-commercial testing and evaluation
+ purposes only. Facebook reserves all rights not expressly granted.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import IGListKit
+
+final class FeedItem: IGListDiffable {
+
+    let pk: Int
+    let user: User
+    let comments: [String]
+
+    init(pk: Int, user: User, comments: [String]) {
+        self.pk = pk
+        self.user = user
+        self.comments = comments
+    }
+
+    //MARK: IGListDiffable
+
+    func diffIdentifier() -> NSObjectProtocol {
+        return pk as NSObjectProtocol
+    }
+
+    func isEqual(_ object: IGListDiffable?) -> Bool {
+        if self === object {
+            return true
+        }
+        guard let object = object as? FeedItem else { return false }
+        return user.isEqual(object.user) && comments == object.comments
+    }
+
+}
---
+++ Example/IGListKitExamples/SectionControllers/FeedItemSectionController.swift
@@ -0,0 +1,69 @@
+/**
+ Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
+
+ The examples provided by Facebook are for non-commercial testing and evaluation
+ purposes only. Facebook reserves all rights not expressly granted.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import IGListKit
+
+final class FeedItemSectionController: IGListSectionController, IGListSectionType, IGListSupplementaryViewSource {
+
+    var feedItem: FeedItem!
+
+    override init() {
+        super.init()
+        supplementaryViewSource = self
+    }
+
+    // MARK: IGlistSectionType
+
+    func numberOfItems() -> Int {
+        return feedItem.comments.count
+    }
+
+    func sizeForItem(at index: Int) -> CGSize {
+        return CGSize(width: collectionContext!.containerSize.width, height: 55)
+    }
+
+    func cellForItem(at index: Int) -> UICollectionViewCell {
+        let cell = collectionContext?.dequeueReusableCell(of: LabelCell.self, for: self, at: index) as! LabelCell
+        cell.label.text = feedItem.comments[index]
+        return cell
+    }
+
+    func didUpdate(to object: Any) {
+        feedItem = object as? FeedItem
+    }
+
+    func didSelectItem(at index: Int) {}
+
+    // MARK: IGListSupplementaryViewSource
+
+    func supportedElementKinds() -> [String] {
+        return [UICollectionElementKindSectionHeader]
+    }
+
+    func viewForSupplementaryElement(ofKind elementKind: String, at index: Int) -> UICollectionReusableView {
+        let view = collectionContext?.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader,
+                                                                       for: self,
+                                                                       nibName: "UserHeaderView",
+                                                                       bundle: nil,
+                                                                       at: index) as! UserHeaderView
+        view.handleLabel.text = "@" + feedItem.user.handle
+        view.nameLabel.text = feedItem.user.name
+        return view
+    }
+
+    func sizeForSupplementaryView(ofKind elementKind: String, at index: Int) -> CGSize {
+        return CGSize(width: collectionContext!.containerSize.width, height: 40)
+    }
+
+}
