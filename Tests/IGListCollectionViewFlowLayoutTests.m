89827ee5eab1c169bacfc7a96bfbaa163cc24d5d
Bofei Zhu <zhubofei@gmail.com>
2016-11-07 23:36:06 -0500

    Update unit test

--- Tests/IGListCollectionViewFlowLayoutTests.m
+++ Tests/IGListCollectionViewFlowLayoutTests.m
@@ -18,6 +18,15 @@
 
 #define genExpectation [self expectationWithDescription:NSStringFromSelector(_cmd)]
 
+#define IGAssertEqualFrame(frame, x, y, w, h, ...) \
+do { \
+CGRect expected = CGRectMake(x, y, w, h); \
+XCTAssertEqual(CGRectGetMinX(expected), CGRectGetMinX(frame)); \
+XCTAssertEqual(CGRectGetMinY(expected), CGRectGetMinY(frame)); \
+XCTAssertEqual(CGRectGetWidth(expected), CGRectGetWidth(frame)); \
+XCTAssertEqual(CGRectGetHeight(expected), CGRectGetHeight(frame)); \
+} while(0)
+
 @interface IGListCollectionViewFlowLayoutTests : XCTestCase
 
 @property (nonatomic, strong) IGListCollectionView *collectionView;
@@ -58,31 +67,25 @@
     [self.collectionView layoutIfNeeded];
 }
 
-- (void)test_whenDisplayingCollectionView_thatSectionsHaveOneItem {
+- (void)test_whenDisplayingCollectionView_thatHasOneItem {
     [self setupWithObjects:@[genTestObject(@1, @"Foo")]];
     CGSize contentSize = self.layout.collectionViewContentSize;
     NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:0];
     UICollectionViewLayoutAttributes *attributes = [self.layout layoutAttributesForItemAtIndexPath:indexPath];
     XCTAssertEqual(contentSize.width, 100.0f);
     XCTAssertEqual(contentSize.height, 44.0f);
-    XCTAssertEqual(CGRectGetMinX(attributes.frame), 0.0f);
-    XCTAssertEqual(CGRectGetMinY(attributes.frame), 0.0f);
-    XCTAssertEqual(CGRectGetWidth(attributes.frame), 100.0f);
-    XCTAssertEqual(CGRectGetHeight(attributes.frame), 44.0f);
+    IGAssertEqualFrame(attributes.frame, 0, 0, 100, 44);
 }
 
-- (void)test_whenDisplayingCollectionView_thatLayouHasItemSize {
+- (void)test_whenDisplayingCollectionView_thatLayoutHasItemSize {
     self.layout.itemSize = CGSizeMake(44.0f, 44.0f);
     [self setupWithObjects:@[genTestObject(@1, @"Foo")]];
     NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:0];
     UICollectionViewLayoutAttributes *attributes = [self.layout layoutAttributesForItemAtIndexPath:indexPath];
-    XCTAssertEqual(CGRectGetMinX(attributes.frame), 0.0f);
-    XCTAssertEqual(CGRectGetMinY(attributes.frame), 0.0f);
-    XCTAssertEqual(CGRectGetWidth(attributes.frame), 44.0f);
-    XCTAssertEqual(CGRectGetHeight(attributes.frame), 44.0f);
+    IGAssertEqualFrame(attributes.frame, 0, 0, 44, 44);
 }
 
-- (void)test_whenDisplayingCollectionView_thatSectionsHaveMultipleItems {
+- (void)test_whenDisplayingCollectionView_thatHasMultipleItems {
     self.layout.itemSize = CGSizeMake(44.0f, 44.0f);
     self.layout.minimumInteritemSpacing = 12.0f;
     self.layout.minimumLineSpacing = 6.0f;
@@ -90,16 +93,45 @@
                              genTestObject(@1, @"Foo"),
                              genTestObject(@2, @"Bar"),
                              genTestObject(@3, @"Baz"),
+                             genTestObject(@4, @"Foo"),
+                             genTestObject(@5, @"Bar"),
+                             genTestObject(@6, @"Baz")
                              ]];
     CGSize contentSize = self.layout.collectionViewContentSize;
-    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:2];
-    UICollectionViewLayoutAttributes *attributes = [self.layout layoutAttributesForItemAtIndexPath:indexPath];
+    NSIndexPath *indexPath0 = [NSIndexPath indexPathForItem:0 inSection:0];
+    NSIndexPath *indexPath2 = [NSIndexPath indexPathForItem:0 inSection:2];
+    NSIndexPath *indexPath3 = [NSIndexPath indexPathForItem:0 inSection:3];
+    NSIndexPath *indexPath5 = [NSIndexPath indexPathForItem:0 inSection:5];
+    UICollectionViewLayoutAttributes *attributes0 = [self.layout layoutAttributesForItemAtIndexPath:indexPath0];
+    UICollectionViewLayoutAttributes *attributes2 = [self.layout layoutAttributesForItemAtIndexPath:indexPath2];
+    UICollectionViewLayoutAttributes *attributes3 = [self.layout layoutAttributesForItemAtIndexPath:indexPath3];
+    UICollectionViewLayoutAttributes *attributes5 = [self.layout layoutAttributesForItemAtIndexPath:indexPath5];
     XCTAssertEqual(contentSize.width, 100.0f);
-    XCTAssertEqual(contentSize.height, 94.0f);
-    XCTAssertEqual(CGRectGetMinX(attributes.frame), 0.0f);
-    XCTAssertEqual(CGRectGetMinY(attributes.frame), 50.0f);
-    XCTAssertEqual(CGRectGetWidth(attributes.frame), 44.0f);
-    XCTAssertEqual(CGRectGetHeight(attributes.frame), 44.0f);
+    XCTAssertEqual(contentSize.height, 144.0f);
+    IGAssertEqualFrame(attributes0.frame, 0, 0, 44, 44);
+    IGAssertEqualFrame(attributes2.frame, 0, 50, 44, 44);
+    IGAssertEqualFrame(attributes3.frame, 56, 50, 44, 44);
+    IGAssertEqualFrame(attributes5.frame, 56, 100, 44, 44);
 }
 
+- (void)test_whenDisplayingCollectionView_thatUsesDelegateSize {
+    self.layout.minimumLineSpacing = 6.0f;
+    [self setupWithObjects:@[
+                             genTestObject(@1, @"Foo"),
+                             genTestObject(@2, @"Bar"),
+                             genTestObject(@3, @"Baz")
+                             ]];
+    CGSize contentSize = self.layout.collectionViewContentSize;
+    NSIndexPath *indexPath0 = [NSIndexPath indexPathForItem:0 inSection:0];
+    NSIndexPath *indexPath1 = [NSIndexPath indexPathForItem:0 inSection:1];
+    NSIndexPath *indexPath2 = [NSIndexPath indexPathForItem:0 inSection:2];
+    UICollectionViewLayoutAttributes *attributes0 = [self.layout layoutAttributesForItemAtIndexPath:indexPath0];
+    UICollectionViewLayoutAttributes *attributes1 = [self.layout layoutAttributesForItemAtIndexPath:indexPath1];
+    UICollectionViewLayoutAttributes *attributes2 = [self.layout layoutAttributesForItemAtIndexPath:indexPath2];
+    XCTAssertEqual(contentSize.width, 100.0f);
+    XCTAssertEqual(contentSize.height, 144.0f);
+    IGAssertEqualFrame(attributes0.frame, 0, 0, 100, 44);
+    IGAssertEqualFrame(attributes1.frame, 0, 50, 100, 44);
+    IGAssertEqualFrame(attributes2.frame, 0, 100, 100, 44);
+}
 @end
