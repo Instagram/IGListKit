94a764b5d6766138e093ccc0d46c3ff947b82c2e
Jesse Squires <jsq@instagram.com>
2016-11-30 06:37:22 -0800

    add example setup script. run pod install.
    Summary:
    - Add script for example setup
    - Run pod install and check-in changes
    
    Reviewed By: dshahidehpour
    
    Differential Revision: D4250015
    
    fbshipit-source-id: 51e7342d698bc9874bbbcfb8e784fe62e82d1ac6

---
+++ Examples/pod_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Setting up iOS examples...'
+cd Examples-iOS/
+pod install
+cd ..
+
+echo 'Setting up tvOS examples...'
+cd Examples-tvOS/
+pod install
+cd ..
+
+echo 'Done!'
+echo ''
