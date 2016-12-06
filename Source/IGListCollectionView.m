394e6c827bf382723057de42e2296af595ce30e3
Dave Caunt <dcaunt@gmail.com>
2016-12-06 06:29:55 -0800

    Don't set UICollectionView properties in IGListCollectionView
    Summary:
    Fixes #277
    Closes https://github.com/Instagram/IGListKit/pull/285
    
    Differential Revision: D4284679
    
    Pulled By: rnystrom
    
    fbshipit-source-id: 6fae3bc928e0065681ebfce906f9e518f819db89

--- Source/IGListCollectionView.m
+++ Source/IGListCollectionView.m
@@ -11,21 +11,10 @@
 
 @implementation IGListCollectionView
 
-- (void)commonInit {
-    self.backgroundColor = [UIColor whiteColor];
-    self.alwaysBounceVertical = YES;
-}
-
 - (instancetype)initWithFrame:(CGRect)frame collectionViewLayout:(UICollectionViewLayout *)layout {
     if (self = [super initWithFrame:frame collectionViewLayout:layout]) {
-        [self commonInit];
-    }
-    return self;
-}
-
-- (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    if (self = [super initWithCoder:aDecoder]) {
-        [self commonInit];
+        self.backgroundColor = [UIColor whiteColor];
+        self.alwaysBounceVertical = YES;
     }
     return self;
 }
