1fa929a79876ef2b4541d8c01c35beaccf319736
Jesse Squires <jesse.d.squires@gmail.com>
2016-12-13 22:08:46 -0800

    setup mac test target + tests

--- Source/IGListBatchUpdateData.mm
+++ Source/IGListBatchUpdateData.mm
@@ -9,6 +9,14 @@
 
 #import "IGListBatchUpdateData.h"
 
+#import <TargetConditionals.h>
+
+#if TARGET_OS_EMBEDDED || TARGET_OS_SIMULATOR
+#import <UIKit/UIKit.h>
+#else
+#import <Cocoa/Cocoa.h>
+#endif
+
 #import <unordered_map>
 
 #import <IGListKit/IGListAssert.h>
