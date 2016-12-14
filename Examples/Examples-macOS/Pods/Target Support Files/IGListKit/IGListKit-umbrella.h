73ff6fcd4aea4ab48f8c5af0c8c681d3396ad55c
Sherlock <james@sherlo.uk>
2016-12-14 19:34:00 +0000

    Ran pod install, and moved IGListAssert.h to Common

---
+++ Examples/Examples-macOS/Pods/Target Support Files/IGListKit/IGListKit-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <Cocoa/Cocoa.h>
+#endif
+
+#import "IGListAssert.h"
+#import "IGListBatchUpdateData.h"
+#import "IGListDiff.h"
+#import "IGListDiffable.h"
+#import "IGListExperiments.h"
+#import "IGListIndexPathResult.h"
+#import "IGListIndexSetResult.h"
+#import "IGListKit.h"
+#import "IGListMacros.h"
+#import "IGListMoveIndex.h"
+#import "IGListMoveIndexPath.h"
+#import "NSNumber+IGListDiffable.h"
+#import "NSString+IGListDiffable.h"
+
+FOUNDATION_EXPORT double IGListKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char IGListKitVersionString[];
+
