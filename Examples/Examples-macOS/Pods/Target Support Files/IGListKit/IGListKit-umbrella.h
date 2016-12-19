ad30e53507ad3c75a098054f4de49a5cd10f562d
Ryan Nystrom <rnystrom@instagram.com>
2016-12-19 11:11:17 -0800

    Move macOS and diffing files to Common dir
    Summary:
    Moving files to `Common` dir so changes are more automated with Buck and easier to manage in the long run. Ran `pod install` on test and example targets. Updated buck file to include `Common` so diffing lib still works.
    
    Part of #270
    
    Reviewed By: jessesquires
    
    Differential Revision: D4346722
    
    fbshipit-source-id: 366d89f6dd571b158b5fccd542080c51517ed341

--- Examples/Examples-macOS/Pods/Target Support Files/IGListKit/IGListKit-umbrella.h
+++ Examples/Examples-macOS/Pods/Target Support Files/IGListKit/IGListKit-umbrella.h
@@ -3,17 +3,7 @@
 #endif
 
 #import "IGListAssert.h"
-#import "IGListIndexSetResult.h"
-#import "IGListDiff.h"
-#import "NSNumber+IGListDiffable.h"
-#import "NSString+IGListDiffable.h"
-#import "IGListMoveIndexPath.h"
-#import "IGListMoveIndex.h"
-#import "IGListIndexPathResult.h"
 #import "IGListBatchUpdateData.h"
-#import "IGListDiffable.h"
-#import "IGListMacros.h"
-#import "IGListExperiments.h"
 #import "IGListKit.h"
 
 FOUNDATION_EXPORT double IGListKitVersionNumber;
--- Examples/Examples-tvOS/Pods/Target Support Files/IGListKit/IGListKit-umbrella.h
+++ Examples/Examples-tvOS/Pods/Target Support Files/IGListKit/IGListKit-umbrella.h
@@ -2,6 +2,20 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "IGListDiff.h"
+#import "IGListDiffable.h"
+#import "IGListExperiments.h"
+#import "IGListIndexPathResult.h"
+#import "IGListIndexSetResult.h"
+#import "IGListMacros.h"
+#import "IGListMoveIndex.h"
+#import "IGListMoveIndexPath.h"
+#import "IGListIndexPathResultInternal.h"
+#import "IGListIndexSetResultInternal.h"
+#import "IGListMoveIndexInternal.h"
+#import "IGListMoveIndexPathInternal.h"
+#import "NSNumber+IGListDiffable.h"
+#import "NSString+IGListDiffable.h"
 #import "IGListAdapter.h"
 #import "IGListAdapterDataSource.h"
 #import "IGListAdapterDelegate.h"
@@ -11,17 +25,9 @@
 #import "IGListBatchUpdateData.h"
 #import "IGListCollectionContext.h"
 #import "IGListCollectionView.h"
-#import "IGListDiff.h"
-#import "IGListDiffable.h"
 #import "IGListDisplayDelegate.h"
-#import "IGListExperiments.h"
 #import "IGListGridCollectionViewLayout.h"
-#import "IGListIndexPathResult.h"
-#import "IGListIndexSetResult.h"
 #import "IGListKit.h"
-#import "IGListMacros.h"
-#import "IGListMoveIndex.h"
-#import "IGListMoveIndexPath.h"
 #import "IGListReloadDataUpdater.h"
 #import "IGListScrollDelegate.h"
 #import "IGListSectionController.h"
@@ -31,8 +37,6 @@
 #import "IGListSupplementaryViewSource.h"
 #import "IGListUpdatingDelegate.h"
 #import "IGListWorkingRangeDelegate.h"
-#import "NSNumber+IGListDiffable.h"
-#import "NSString+IGListDiffable.h"
 
 FOUNDATION_EXPORT double IGListKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char IGListKitVersionString[];
