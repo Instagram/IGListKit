3fd4ee9630b33780a88c1d9a3a016b1cd968efe2
Sherlock <james@sherlo.uk>
2016-12-24 16:15:35 +0000

    Added compatibility header

--- Source/Common/IGListBatchUpdateData.mm
+++ Source/Common/IGListBatchUpdateData.mm
@@ -8,14 +8,7 @@
  */
 
 #import "IGListBatchUpdateData.h"
-
-#import <TargetConditionals.h>
-
-#if TARGET_OS_EMBEDDED || TARGET_OS_SIMULATOR
-#import <UIKit/UIKit.h>
-#else
-#import <Cocoa/Cocoa.h>
-#endif
+#import <IGListKit/IGListCompatibility.h>
 
 #import <unordered_map>
 
--- Source/Common/IGListKit.h
+++ Source/Common/IGListKit.h
@@ -7,13 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#import <TargetConditionals.h>
-
-#if TARGET_OS_EMBEDDED || TARGET_OS_SIMULATOR
-#import <UIKit/UIKit.h>
-#else
-#import <Cocoa/Cocoa.h>
-#endif
+#import <IGListKit/IGListCompatibility.h>
 
 /**
  * Project version number for IGListKit.
