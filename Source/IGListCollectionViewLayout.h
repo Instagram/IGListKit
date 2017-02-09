29dd1fe6f941e811631f92cb732efcaa47a090bf
Ryan Nystrom <rnystrom@fb.com>
2017-02-09 14:34:33 -0500

    nits

--- Source/IGListCollectionViewLayout.h
+++ Source/IGListCollectionViewLayout.h
@@ -25,34 +25,42 @@ NS_ASSUME_NONNULL_BEGIN
 
  All UICollectionViewDelegateFlowLayout methods are required and used by this layout:
 
+ ```
  - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath;
  - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section;
  - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section;
  - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section;
  - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section;
+ ```
 
  Sections and items are put into the same horizontal row until the max-x position of an item extends beyond the width
  of the collection view. When that happens, the item is "newlined" to the next row. The y position of that row is
  deteremined by the maximum height (including section insets) of the section/item of the previous row.
 
  Ex. of a section (2,0) with a large width causing a newline.
+ ```
  |[ 0,0 ][ 1,0 ]         |
  |[         2,0         ]|
+ ```
 
  A section with a non-zero height header will always cause that section to newline. Headers are always stretched to the
  width of the collection view, pinched with the section insets.
 
  Ex. of a section (2,0) with a header inset on the left/right.
+ ```
  |[ 0,0 ][ 1,0 ]         |
  | >======header=======< |
  | [ 2,0 ]               |
+ ```
 
  Section insets apply to items in the section no matter if they begin on a new row or are on the same row as a previous
  section.
 
  Ex. of a section (2) with multiple items and a left inset.
+ ```
  |[ 0,0 ][ 1,0 ] >[ 2,0 ]|
  | >[ 2,1 ][ 2,2 ][ 2,3 ]|
+ ```
 
  Interitem spacing applies to items and sections within the same row. Line spacing only applies to items within the same
  section.
@@ -67,14 +75,14 @@ IGLK_SUBCLASSING_RESTRICTED
  navigation bar is scrolled offscreen. Changing this to the height of the navigation bar will give the effect of the
  headers sticking to the nav as it is collapsed.
 
- @discussion Changing the value on this method will invalidat the layout every time.
+ @discussion Changing the value on this method will invalidate the layout every time.
  */
 @property (nonatomic, assign) CGFloat stickyHeaderOriginYAdjustment;
 
 /**
  Create and return a new collection view layout.
- @param stickyHeaders       Set to YES to stick section headers to the top of the bounds while scrolling.
- @param topContentInset     The top content inset used to offset the sticky headers. Ignored if stickyHeaders is NO.
+ @param stickyHeaders       Set to `YES` to stick section headers to the top of the bounds while scrolling.
+ @param topContentInset     The top content inset used to offset the sticky headers. Ignored if stickyHeaders is `NO`.
  @return A new collection view layout.
  */
 - (instancetype)initWithStickyHeaders:(BOOL)stickyHeaders
