9a3851a0465fdd297ba0f70587b9303d806d82a7
Ryan Nystrom <rnystrom@instagram.com>
2017-04-03 17:14:28 -0400

    Improve the ObjC demo to reflect best-practices

--- Examples/Examples-iOS/IGListKitExamples/ViewControllers/ObjcDemoViewController.m
+++ Examples/Examples-iOS/IGListKitExamples/ViewControllers/ObjcDemoViewController.m
@@ -16,74 +16,54 @@
 
 #import <IGListKit/IGListKit.h>
 
-#import "UserInfoSectionController.h"
-#import "ImageSectionController.h"
-#import "InteractiveSectionController.h"
-#import "CommentSectionController.h"
-
-#import "UserInfo.h"
-#import "PhotoCell.h"
+#import "Post.h"
+#import "PostSectionController.h"
 
 @interface ObjcDemoViewController () <IGListAdapterDataSource>
 
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) IGListAdapter *adapter;
-@property (nonatomic, strong) NSArray *data;
+@property (nonatomic, strong) NSArray<Post *> *data;
 
 @end
 
 
 @implementation ObjcDemoViewController
 
-#pragma mark - Setup
+- (void)viewDidLoad {
+    [super viewDidLoad];
 
-- (void)setupUI {
-    UICollectionViewLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
+    self.data = @[
+                  [[Post alloc] initWithUsername:@"userA" comments:@[
+                                                                     @"Luminous triangle",
+                                                                     @"Awesome",
+                                                                     @"Super clean",
+                                                                     @"Stunning shot",
+                                                                     ]],
+                  [[Post alloc] initWithUsername:@"userB" comments:@[
+                                                                     @"The simplicity here is superb",
+                                                                     @"thanks!",
+                                                                     @"That's always so kind of you!",
+                                                                     @"I think you might like this",
+                                                                     ]],
+                  [[Post alloc] initWithUsername:@"userC" comments:@[
+                                                                     @"So good",
+                                                                     ]],
+                  [[Post alloc] initWithUsername:@"userD" comments:@[
+                                                                     @"hope she might like it.",
+                                                                     @"I love it."
+                                                                     ]],
+                  ];
+
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero
+                                             collectionViewLayout:[UICollectionViewFlowLayout new]];
     [self.view addSubview:self.collectionView];
-
     self.adapter = [[IGListAdapter alloc] initWithUpdater:[[IGListAdapterUpdater alloc] init]
                                            viewController:self
                                          workingRangeSize:0];
 
     self.adapter.collectionView = self.collectionView;
     self.adapter.dataSource = self;
-
-    UserInfo *userA = [[UserInfo alloc] initWithName:@"userA"];
-    UserInfo *userB = [[UserInfo alloc] initWithName:@"userB"];
-    UserInfo *userC = [[UserInfo alloc] initWithName:@"userC"];
-    UserInfo *userD = [[UserInfo alloc] initWithName:@"userD"];
-
-    self.data = @[ userA,
-                   @"Image-Placeholder-String",
-                   @"", @"Luminous triangle",
-                   @"Awesome",
-                   @"Super clean",
-                   @"Stunning shot",
-                   userB,
-                   @"Image-Placeholder-String",
-                   @"",
-                   @"The simplicity here is superb",
-                   @"thanks!", @"That's always so kind of you!",
-                   @"I think you might like this",
-                   userC,
-                   @"Image-Placeholder-String",
-                   @"",
-                   @"So good comment",
-                   userD,
-                   @"Image-Placeholder-String",
-                   @"",
-                   @"hope she might like it.",
-                   @"I love it."
-                   ];
-
-}
-
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    [self setupUI];
 }
 
 - (void)viewDidLayoutSubviews {
@@ -98,19 +78,7 @@
 }
 
 - (IGListSectionController<IGListSectionType> *)listAdapter:(IGListAdapter *)listAdapter sectionControllerForObject:(id)object {
-    if ([object isKindOfClass:[NSString class]]) {
-        if ([object isEqualToString:@"Image-Placeholder-String"]) {
-            return [[ImageSectionController alloc] init];
-        } else if ([object length]) {
-            return [[CommentSectionController alloc] init];
-        } else {
-            return [[InteractiveSectionController alloc] init];
-        }
-    } else {
-        return [[UserInfoSectionController alloc] init];
-    }
-
-    return nil;
+    return [PostSectionController new];
 }
 
 - (UIView *)emptyViewForListAdapter:(IGListAdapter *)listAdapter {
