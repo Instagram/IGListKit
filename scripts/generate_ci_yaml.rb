f0056953385a606b12badf35e03cd3a30ad53858
heshamsalman <hesham.salman@viewthespace.com>
2017-04-16 21:53:59 -0400

    Responded to comments
    Rebased on master

--- scripts/generate_ci_yaml.rb
+++ scripts/generate_ci_yaml.rb
@@ -1,5 +1,5 @@
 require 'yaml'
 
-data = YAML.load_file ".swiftlint.yml"
+data = YAML.load_file "Examples/.swiftlint.yml"
 data.delete("included")
 File.open(".swiftlint_CI.yml", 'w') { |f| f.write data.to_yaml.gsub('---', '') }
