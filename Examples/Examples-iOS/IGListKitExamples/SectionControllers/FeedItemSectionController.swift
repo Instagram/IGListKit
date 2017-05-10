f2193410c8a9e632b0e3d0cc27d0a149c3cb6966
heshamsalman <hesham.salman@viewthespace.com>
2017-05-10 14:24:32 -0400

    Deleted file.zip

--- Examples/Examples-iOS/IGListKitExamples/SectionControllers/FeedItemSectionController.swift
+++ Examples/Examples-iOS/IGListKitExamples/SectionControllers/FeedItemSectionController.swift
@@ -34,7 +34,9 @@ final class FeedItemSectionController: ListSectionController, ListSupplementaryV
     }
 
     override func cellForItem(at index: Int) -> UICollectionViewCell {
-        let cell = collectionContext?.dequeueReusableCell(of: LabelCell.self, for: self, at: index) as! LabelCell
+        guard let cell = collectionContext?.dequeueReusableCell(of: LabelCell.self, for: self, at: index) as? LabelCell else {
+            fatalError()
+        }
         cell.text = feedItem.comments[index]
         return cell
     }
