47dbfcb867e75e6abd60a40d8ed0bbb09a796195
Weyert de Boer <wdb@innerfuse.biz>
2017-06-26 01:22:16 +0200

    Created a more charming example out of the supplementary views example so it uses sticky headers

--- Examples/Examples-iOS/IGListKitExamples/ViewControllers/SupplementaryViewController.swift
+++ Examples/Examples-iOS/IGListKitExamples/ViewControllers/SupplementaryViewController.swift
@@ -1,9 +1,9 @@
 /**
  Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
-
+ 
  The examples provided by Facebook are for non-commercial testing and evaluation
  purposes only. Facebook reserves all rights not expressly granted.
-
+ 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -16,41 +16,73 @@ import UIKit
 import IGListKit
 
 final class SupplementaryViewController: UIViewController, ListAdapterDataSource {
-
+    
     lazy var adapter: ListAdapter = {
         return ListAdapter(updater: ListAdapterUpdater(), viewController: self)
     }()
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: ListCollectionViewLayout(stickyHeaders: true, topContentInset: 0.0, stretchToEdge: false))
+    
     let feedItems = [
-        FeedItem(pk: 1, user: User(pk: 100, name: "Jesse", handle: "jesse_squires"), comments: ["You rock!", "Hmm you sure about that?"]),
-        FeedItem(pk: 2, user: User(pk: 101, name: "Ryan", handle: "_ryannystrom"), comments: ["lgtm", "lol", "Let's try it!"]),
-        FeedItem(pk: 3, user: User(pk: 102, name: "Ann", handle: "abaum"), comments: ["Good luck!"]),
-        FeedItem(pk: 4, user: User(pk: 103, name: "Phil", handle: "phil"), comments: ["yoooooooo", "What's the eta?"])
+        FeedItem(pk: 1, user: User(pk: 100, name: "Jesse", handle: "jesse_squires"), comments: [
+            "You rock!",
+            "Hmm you sure about that?",
+            "You rock!",
+            "Hmm you sure about that?",
+            "You rock!",
+            "Hmm you sure about that?"
+            ]),
+        FeedItem(pk: 2, user: User(pk: 101, name: "Ryan", handle: "_ryannystrom"), comments: [
+            "lgtm",
+            "lol",
+            "Let's try it!",
+            "lol",
+            "Let's try it!",
+            "Good luck!"
+            ]),
+        FeedItem(pk: 3, user: User(pk: 102, name: "Ann", handle: "abaum"), comments: [
+            "Good luck!",
+            "yoooooooo",
+            "lol"
+            ]),
+        FeedItem(pk: 4, user: User(pk: 103, name: "Phil", handle: "phil"), comments: [
+            "yoooooooo",
+            "What's the eta?"
+            ])
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let flowLayout = collectionView.collectionViewLayout as? ListCollectionViewLayout {
+            let statusBarSize = UIApplication.shared.statusBarFrame.size
+            let navigationBarVisible = self.navigationController?.isNavigationBarHidden == false
+            let navigationBarHeight = ( navigationBarVisible ? self.navigationController!.navigationBar.frame.height : 0 )
+            
+            // If we are showing a navigation bar we need to change the y offset for the sticky headers as normal behaviour
+            // of the UICollectionView to keep scrolling under the navigation bar. This case the sticky headers to end up below
+            // this bar too hence this bit of calculation to determine what the correct y offset is
+            flowLayout.stickyHeaderOriginYAdjustment = navigationBarHeight + statusBarSize.height
+            collectionView.collectionViewLayout = flowLayout
+        }
         view.addSubview(collectionView)
         adapter.collectionView = collectionView
         adapter.dataSource = self
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         collectionView.frame = view.bounds
     }
-
+    
     // MARK: ListAdapterDataSource
-
+    
     func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
         return feedItems
     }
-
+    
     func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
         return FeedItemSectionController()
     }
-
+    
     func emptyView(for listAdapter: ListAdapter) -> UIView? { return nil }
-
+    
 }
