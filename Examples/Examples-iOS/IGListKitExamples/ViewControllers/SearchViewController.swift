33f88b43e8ede36e6be5efeddf54d4000c05bf9a
Weyert de Boer <wdb@innerfuse.biz>
2017-06-29 17:55:31 -0700

    Fixed the issue with the Search example not working
    Summary:
    I have updated the Search Autocomplete example which stopped working as the words don't have unique `diffIdentifier`'s. This caused by the check that good introduced to ensure all objects assigned to a section controller are unique. Fixed this issue by removing all the double words.
    
    - [X] All tests pass. Demo project builds and runs.
    - [ ] I added tests, an experiment, or detailed why my change isn't tested.
    - [ ] I added an entry to the `CHANGELOG.md` for any breaking changes, enhancements, or bug fixes.
    - [X] I have reviewed the [contributing guide](https://github.com/Instagram/IGListKit/blob/master/.github/CONTRIBUTING.md)
    Closes https://github.com/Instagram/IGListKit/pull/835
    
    Differential Revision: D5353304
    
    Pulled By: rnystrom
    
    fbshipit-source-id: 78cb394469832bd77df7e0cd455a24e46671ba69

--- Examples/Examples-iOS/IGListKitExamples/ViewControllers/SearchViewController.swift
+++ Examples/Examples-iOS/IGListKitExamples/ViewControllers/SearchViewController.swift
@@ -21,12 +21,13 @@ final class SearchViewController: UIViewController, ListAdapterDataSource, Searc
         return ListAdapter(updater: ListAdapterUpdater(), viewController: self)
     }()
     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-    lazy var words: [String] = {
+    lazy var words: Set<String> = {
+        // swiftlint:disable:next
         let str = "Humblebrag skateboard tacos viral small batch blue bottle, schlitz fingerstache etsy squid. Listicle tote bag helvetica XOXO literally, meggings cardigan kickstarter roof party deep v selvage scenester venmo truffaut. You probably haven't heard of them fanny pack austin next level 3 wolf moon. Everyday carry offal brunch 8-bit, keytar banjo pinterest leggings hashtag wolf raw denim butcher. Single-origin coffee try-hard echo park neutra, cornhole banh mi meh austin readymade tacos taxidermy pug tattooed. Cold-pressed +1 ethical, four loko cardigan meh forage YOLO health goth sriracha kale chips. Mumblecore cardigan humblebrag, lo-fi typewriter truffaut leggings health goth."
-        var words = [String]()
+        var words = Set<String>()
         let range = str.startIndex ..< str.endIndex
         str.enumerateSubstrings(in: range, options: .byWords) { (substring, _, _, _) in
-            words.append(substring!)
+            words.insert(substring!)
         }
         return words
     }()
