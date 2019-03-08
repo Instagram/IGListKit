f860c15e7f6363bdcdba182f87e24cdc7e897e4c
Zhisheng Huang <zsh@fb.com>
2019-03-08 09:55:33 -0800

    Cleanup the coalescing time experiment
    Summary:
    We have observed that keeping the coalescing time here would actually decrease the stability here, it makes the app crash more often.
    It's probably caused by some race condition during that time and UICV updates become out of sync.
    
    Reviewed By: rnystrom
    
    Differential Revision: D14379265
    
    fbshipit-source-id: 502c1c14fb8bdfc35969f721687e82888b160110

--- Source/IGListAdapterUpdater.m
+++ Source/IGListAdapterUpdater.m
@@ -427,15 +427,19 @@ static NSArray<NSIndexPath *> *convertSectionReloadToItemUpdates(NSIndexSet *sec
 
 - (void)_queueUpdateWithCollectionViewBlock:(IGListCollectionViewBlock)collectionViewBlock {
     IGAssertMainThread();
+    
     __weak __typeof__(self) weakSelf = self;
-
-    // dispatch after a given amount of time to coalesce other updates and execute as one
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, self.coalescanceTime * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+    
+    // dispatch_async to give the main queue time to collect more batch updates so that a minimum amount of work
+    // (diffing, etc) is done on main. dispatch_async does not garauntee a full runloop turn will pass though.
+    // see -performUpdateWithCollectionView:fromObjects:toObjects:animated:objectTransitionBlock:completion: for more
+    // details on how coalescence is done.
+    dispatch_async(dispatch_get_main_queue(), ^{
         if (weakSelf.state != IGListBatchUpdateStateIdle
             || ![weakSelf hasChanges]) {
             return;
         }
-
+        
         if (weakSelf.hasQueuedReloadData) {
             [weakSelf performReloadDataWithCollectionViewBlock:collectionViewBlock];
         } else {
