3669b2ec873024e3af9e156acc985fd56fa1c044
Maxime Ollivier <maxo@instagram.com>
2019-04-30 10:44:00 -0700

    clean up IGListExperimentBackgroundDiffingSerial
    Summary: * Cleaning up experiment
    
    Reviewed By: lorixx
    
    Differential Revision: D15134137
    
    fbshipit-source-id: a3f5a6d7b4a5f3897608c4e32ad4c4e6e4e720e5

--- Source/Common/IGListExperiments.h
+++ Source/Common/IGListExperiments.h
@@ -26,8 +26,6 @@ typedef NS_OPTIONS (NSInteger, IGListExperiment) {
     IGListExperimentGetCollectionViewAtUpdate = 1 << 7,
     /// Test skipping layout when UICollectionView isn't visible
     IGListExperimentSkipLayout = 1 << 8,
-    /// Test updater diffing performed on a serial background queue.
-    IGListExperimentBackgroundDiffingSerial = 1 << 9,
 };
 
 /**
