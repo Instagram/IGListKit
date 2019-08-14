7af667aefcdfc2a28be5bdd16599b67db4e04e17
Markus Emrich <emrich@fb.com>
2019-08-14 12:20:36 -0700

    Update remodel iglistdiffable plugin to the latest
    Summary:
    Remodel changed quite a bit, this updates the plugin to work correctly with the latest state.
    It also includes other recent Remodel changes like e.g. autoformatting of the codebase.
    
    Resolves #1353
    
    Reviewed By: natestedman
    
    Differential Revision: D16794236
    
    fbshipit-source-id: f543d0e3e5656f540accd5e359bbaf0acc8071fc

--- remodel-plugin/src/plugins/iglistdiffable-utils.ts
+++ remodel-plugin/src/plugins/iglistdiffable-utils.ts
@@ -5,19 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import Maybe = require('../maybe');
-import ObjC = require('../objc');
-import ObjCTypeUtils = require('../objc-type-utils');
-import ObjectSpec = require('../object-spec');
-import ObjectSpecCodeUtils = require('../object-spec-code-utils');
+import * as Maybe from '../maybe';
+import * as ObjC from '../objc';
+import * as ObjCTypeUtils from '../objc-type-utils';
+import * as ObjectSpec from '../object-spec';
+import * as ObjectSpecCodeUtils from '../object-spec-code-utils';
 
-function isEqualToDiffableObjectMethod():ObjC.Method {
+function isEqualToDiffableObjectMethod(): ObjC.Method {
   return {
-    preprocessors:[],
-    belongsToProtocol:Maybe.Just<string>('IGListDiffable'),
+    preprocessors: [],
+    belongsToProtocol: Maybe.Just<string>('IGListDiffable'),
     code: ['return [self isEqual:object];'],
-    comments:[],
-    compilerAttributes:[],
+    comments: [],
+    compilerAttributes: [],
     keywords: [
       {
         name: 'isEqualToDiffableObject',
@@ -26,110 +26,142 @@ function isEqualToDiffableObjectMethod():ObjC.Method {
           modifiers: [ObjC.KeywordArgumentModifier.Nullable()],
           type: {
             name: 'id',
-            reference: 'id'
-          }
-        })
-      }
+            reference: 'id',
+          },
+        }),
+      },
     ],
-    returnType: { type: Maybe.Just({
-      name: 'BOOL',
-      reference: 'BOOL'
-    }), modifiers: [] }
-  }
+    returnType: {
+      type: Maybe.Just({
+        name: 'BOOL',
+        reference: 'BOOL',
+      }),
+      modifiers: [],
+    },
+  };
 }
 
-function functionReturnValueForIvarWithFunctionName(iVarString:string, functionToCall:string):string {
+function functionReturnValueForIvarWithFunctionName(
+  iVarString: string,
+  functionToCall: string,
+): string {
   return functionToCall + '(' + iVarString + ')';
 }
 
-function formattedStringValueForIvarWithFormatSpecifier(iVarString:string, stringFormatSpecifier:string, optionalCast:string=null):string {
-  var castString:string = (optionalCast === null ? "" : "(" + optionalCast + ")");
-  return "[NSString stringWithFormat:@\"" + stringFormatSpecifier + "\", " + castString + iVarString + "]";
+function formattedStringValueForIvarWithFormatSpecifier(
+  iVarString: string,
+  stringFormatSpecifier: string,
+  optionalCast: string = null,
+): string {
+  var castString: string =
+    optionalCast === null ? '' : '(' + optionalCast + ')';
+  return (
+    '[NSString stringWithFormat:@"' +
+    stringFormatSpecifier +
+    '", ' +
+    castString +
+    iVarString +
+    ']'
+  );
 }
 
-function nullableObjectValueWithFallback(objectValue:string, optionalFallback:string=null) {
-  return (optionalFallback === null) ? objectValue : `${objectValue} ?: ${optionalFallback}`;
+function nullableObjectValueWithFallback(
+  objectValue: string,
+  optionalFallback: string = null,
+) {
+  return optionalFallback === null
+    ? objectValue
+    : `${objectValue} ?: ${optionalFallback}`;
 }
 
-function wrappedInNSValueForTypeName(iVarString:string, typeName:string) {
+function wrappedInNSValueForTypeName(iVarString: string, typeName: string) {
   return `[NSValue valueWith${typeName}:${iVarString}]`;
 }
 
-function objectValueForAttribute(attribute:ObjectSpec.Attribute, optionalFallback:string=null):string {
-  const iVarString:string = ObjectSpecCodeUtils.ivarForAttribute(attribute);
-  const type:ObjC.Type = ObjectSpecCodeUtils.computeTypeOfAttribute(attribute);
+function objectValueForAttribute(
+  attribute: ObjectSpec.Attribute,
+  optionalFallback: string = null,
+): string {
+  const iVarString: string = ObjectSpecCodeUtils.ivarForAttribute(attribute);
+  const type: ObjC.Type = ObjectSpecCodeUtils.computeTypeOfAttribute(attribute);
 
-  return ObjCTypeUtils.matchType({
-    id: function() {
-      return formattedStringValueForIvarWithFormatSpecifier(iVarString, "%@");
+  return ObjCTypeUtils.matchType(
+    {
+      id: function() {
+        return formattedStringValueForIvarWithFormatSpecifier(iVarString, '%@');
+      },
+      NSObject: function() {
+        return nullableObjectValueWithFallback(iVarString, optionalFallback);
+      },
+      BOOL: function() {
+        return `@(${iVarString})`;
+      },
+      NSInteger: function() {
+        return `@(${iVarString})`;
+      },
+      NSUInteger: function() {
+        return `@(${iVarString})`;
+      },
+      double: function() {
+        return `@(${iVarString})`;
+      },
+      float: function() {
+        return `@(${iVarString})`;
+      },
+      CGFloat: function() {
+        return `@(${iVarString})`;
+      },
+      NSTimeInterval: function() {
+        return `@(${iVarString})`;
+      },
+      uintptr_t: function() {
+        return `@(${iVarString})`;
+      },
+      uint32_t: function() {
+        return `@(${iVarString})`;
+      },
+      uint64_t: function() {
+        return `@(${iVarString})`;
+      },
+      int32_t: function() {
+        return `@(${iVarString})`;
+      },
+      int64_t: function() {
+        return `@(${iVarString})`;
+      },
+      SEL: function() {
+        return functionReturnValueForIvarWithFunctionName(
+          iVarString,
+          'NSStringFromSelector',
+        );
+      },
+      NSRange: function() {
+        return wrappedInNSValueForTypeName(iVarString, 'Range');
+      },
+      CGRect: function() {
+        return wrappedInNSValueForTypeName(iVarString, type.name);
+      },
+      CGPoint: function() {
+        return wrappedInNSValueForTypeName(iVarString, type.name);
+      },
+      CGSize: function() {
+        return wrappedInNSValueForTypeName(iVarString, type.name);
+      },
+      UIEdgeInsets: function() {
+        return wrappedInNSValueForTypeName(iVarString, type.name);
+      },
+      Class: function() {
+        return formattedStringValueForIvarWithFormatSpecifier(iVarString, '%@');
+      },
+      dispatch_block_t: function() {
+        return formattedStringValueForIvarWithFormatSpecifier(iVarString, '%@');
+      },
+      unmatchedType: function() {
+        return nullableObjectValueWithFallback('self', optionalFallback);
+      },
     },
-    NSObject: function() {
-      return nullableObjectValueWithFallback(iVarString, optionalFallback);
-    },
-    BOOL: function() {
-      return `@(${iVarString})`;
-    },
-    NSInteger: function() {
-      return `@(${iVarString})`;
-    },
-    NSUInteger: function() {
-      return `@(${iVarString})`;
-    },
-    double: function() {
-      return `@(${iVarString})`;
-    },
-    float: function() {
-      return `@(${iVarString})`;
-    },
-    CGFloat: function() {
-      return `@(${iVarString})`;
-    },
-    NSTimeInterval: function() {
-      return `@(${iVarString})`;
-    },
-    uintptr_t: function() {
-      return `@(${iVarString})`;
-    },
-    uint32_t: function() {
-      return `@(${iVarString})`;
-    },
-    uint64_t: function() {
-      return `@(${iVarString})`;
-    },
-    int32_t: function() {
-      return `@(${iVarString})`;
-    },
-    int64_t: function() {
-      return `@(${iVarString})`;
-    },
-    SEL: function() {
-      return functionReturnValueForIvarWithFunctionName(iVarString, "NSStringFromSelector");
-    },
-    NSRange: function() {
-      return wrappedInNSValueForTypeName(iVarString, 'Range');
-    },
-    CGRect: function() {
-      return wrappedInNSValueForTypeName(iVarString, type.name);
-    },
-    CGPoint: function() {
-      return wrappedInNSValueForTypeName(iVarString, type.name);
-    },
-    CGSize: function() {
-      return wrappedInNSValueForTypeName(iVarString, type.name);
-    },
-    UIEdgeInsets: function() {
-      return wrappedInNSValueForTypeName(iVarString, type.name);
-    },
-    Class: function() {
-      return formattedStringValueForIvarWithFormatSpecifier(iVarString, "%@");
-    },
-    dispatch_block_t: function() {
-      return formattedStringValueForIvarWithFormatSpecifier(iVarString, "%@");
-    },
-    unmatchedType: function() {
-      return nullableObjectValueWithFallback('self', optionalFallback);
-    }
-  }, type);
+    type,
+  );
 }
 
 export {isEqualToDiffableObjectMethod, objectValueForAttribute};
