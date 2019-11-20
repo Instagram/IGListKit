7ed41e9b532e52d2b0571794a6561961b9d8a6c5
Koen Punt <koen@koenpunt.nl>
2019-11-20 23:00:09 +0100

    add github actions CI workflow

--- scripts/pod_setup.sh
+++ scripts/pod_setup.sh
@@ -3,20 +3,20 @@
 echo 'Setting up iOS examples...'
 cd Examples/Examples-iOS/
 bundle exec pod install
-echo ''
-cd ../..
+echo
+cd -
 
 echo 'Setting up tvOS examples...'
 cd Examples/Examples-tvOS/
 bundle exec pod install
-echo ''
-cd ../..
+echo
+cd -
 
 echo 'Setting up macOS examples...'
 cd Examples/Examples-macOS/
 bundle exec pod install
-echo ''
-cd ../..
+echo 
+cd -
 
 echo 'Done!'
-echo ''
+echo
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,10 +4,10 @@ echo 'Running bundle install...'
 bundle install
 
 echo 'Running pod install...'
-pod install
+bundle exec pod install
 
 echo 'Setting up example projects...'
 ./scripts/pod_setup.sh
 
 echo 'Done!'
-echo ''
+echo
