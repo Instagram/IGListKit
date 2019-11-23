86e103677863f10617f3e4677a54caf32c512f07
Jefferson Setiawan <jefferson.setiawan@icloud.com>
2019-11-23 17:25:55 +0700

    remove IGListKit import, add UIKit import

--- Source/IGListKit/Internal/IGListBatchUpdateData+DebugDescription.h
+++ Source/IGListKit/Internal/IGListBatchUpdateData+DebugDescription.h
@@ -5,7 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <IGListKit/IGListKit.h>
+#import <UIKit/UIKit.h>
+
 #import <IGListDiffKit/IGListBatchUpdateData.h>
 
 @interface IGListBatchUpdateData (DebugDescription)
