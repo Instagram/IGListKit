b2c0ca4efd8eed6635acda144419c36dc3668057
rbbtsn0w <hamiltonsnow@gmail.com>
2019-12-16 15:48:24 +0800

    Fix macOS 10.15 defaults cmd

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,4 +6,4 @@
 
 cd "$(dirname "$(dirname "$0")")" || exit 1
 
-exec defaults read "$(pwd)/Source/Info" CFBundleShortVersionString
+exec plutil -extract CFBundleShortVersionString xml1 -o - "$(pwd)/Source/Info.plist" | sed -n "s/.*<string>\(.*\)<\/string>.*/\1/p"
