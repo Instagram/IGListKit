bcbb10434cdd4a243a93c8781cf7a1fa2e9e3148
Benny Wong <bennywong@fb.com>
2020-02-03 11:32:05 -0500

    [Assert] Add missing IGAssertNonnull function
    * This was introduced in https://github.com/instagram/IGListKit/commit/b4dc116e
    * This is currently breaking our CI build

--- Source/IGListDiffKit/IGListAssert.h
+++ Source/IGListDiffKit/IGListAssert.h
@@ -20,3 +20,19 @@
 #ifndef IGAssertMainThread
 #define IGAssertMainThread() IGAssert( ([NSThread isMainThread] == YES), @"Must be on the main thread")
 #endif // IGAssertMainThread
+
+#ifndef IGAssertNonnull
+/// Takes in a potentially-nullable value, asserts that it is not null, and "returns" it as a nonnull
+/// value. If the value is `nil`, this is technically unsafe, so use of this macro should be limited
+/// to cases where we definitely expect a value to be nonnull, and would be very surprised if it is
+/// nil. This is intended to allow us to incrementally improve the nullability coverage and handling
+/// in our codebase as we move towards Swift.
+#define IGAssertNonnull(value) ({ \
+    /* evaluate the value only once, to avoid repeated side-effects */ \
+    __typeof(*(value)) *__nonnull IGAssertNonnull_unwrapped_value = (id)(value); \
+    /* stringify the value expression with # to embed in assertion message */ \
+    IGAssert(IGAssertNonnull_unwrapped_value, @"%s was unexpectedly nil", #value); \
+    /* "return" the value to the caller */ \
+    IGAssertNonnull_unwrapped_value; \
+})
+#endif // IGAssertNonnull
