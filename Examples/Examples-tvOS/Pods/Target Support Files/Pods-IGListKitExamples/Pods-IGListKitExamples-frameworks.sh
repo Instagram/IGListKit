847a67fa142ba6257c1586e73fd786d8bc21882a
Zhisheng Huang <zsh@fb.com>
2020-02-26 12:22:32 -0800

    Run ./script/setup.sh to generate project files (#1431)
    Summary:
    ## Changes in this pull request
    
    Run ./script/setup.sh to regenerate some of the Example projects missing files, that are causing the build failure in travis CI. Let's see how it goes.
    
    Issue fixed: #
    
    ### Checklist
    
    - [ ] All tests pass. Demo project builds and runs.
    - [ ] I added tests, an experiment, or detailed why my change isn't tested.
    - [ ] I added an entry to the `CHANGELOG.md` for any breaking changes, enhancements, or bug fixes.
    - [ ] I have reviewed the [contributing guide](https://github.com/Instagram/IGListKit/blob/master/.github/CONTRIBUTING.md)
    Pull Request resolved: https://github.com/Instagram/IGListKit/pull/1431
    
    Reviewed By: bdotdub
    
    Differential Revision: D20119287
    
    Pulled By: lorixx
    
    fbshipit-source-id: c9b22b513a248c6428f4730f620f8d72ff37a560

--- Examples/Examples-tvOS/Pods/Target Support Files/Pods-IGListKitExamples/Pods-IGListKitExamples-frameworks.sh
+++ Examples/Examples-tvOS/Pods/Target Support Files/Pods-IGListKitExamples/Pods-IGListKitExamples-frameworks.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 set -e
 set -u
 set -o pipefail
