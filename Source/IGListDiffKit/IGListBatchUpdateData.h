315216328b0f3669ec37f5ecfd6a4738d1022ba1
Maxime Ollivier <maxo@fb.com>
2020-06-02 13:02:01 -0700

    remove IGListExperimentFixIndexPathImbalance
    Summary: Actually this won't work and causes even more crashes. For example, if we reload cell 0 and insert at 0, we end up with just a single delete & insert at 0, which causes an inconsistency exception. `IGListApplyUpdatesToCollectionView` already takes care of avoiding multiple reloads of the same `NSIndexPath` by using a `NSMutableSet` when collecting the `reloadDeletePaths`.
    
    Reviewed By: apadalko
    
    Differential Revision: D21822502
    
    fbshipit-source-id: c2382951ffe013f82c1de2492286d2911f40444d

--- Source/IGListDiffKit/IGListBatchUpdateData.h
+++ Source/IGListDiffKit/IGListBatchUpdateData.h
@@ -67,7 +67,6 @@ NS_SWIFT_NAME(ListBatchUpdateData)
  @param deleteIndexPaths Item index paths to delete.
  @param updateIndexPaths Item index paths to update.
  @param moveIndexPaths Item index paths to move.
- @param fixIndexPathImbalance When enabled, we remove duplicate NSIndexPath inserts to avoid insert/delete imbalance and a crash.
 
  @return A new batch update object.
  */
@@ -77,19 +76,7 @@ NS_SWIFT_NAME(ListBatchUpdateData)
                       insertIndexPaths:(NSArray<NSIndexPath *> *)insertIndexPaths
                       deleteIndexPaths:(NSArray<NSIndexPath *> *)deleteIndexPaths
                       updateIndexPaths:(NSArray<NSIndexPath *> *)updateIndexPaths
-                        moveIndexPaths:(NSArray<IGListMoveIndexPath *> *)moveIndexPaths
-                 fixIndexPathImbalance:(BOOL)fixIndexPathImbalance NS_DESIGNATED_INITIALIZER;
-
-/**
- Convenience initializer with fixIndexPathImbalance disabled.
- */
-- (instancetype)initWithInsertSections:(NSIndexSet *)insertSections
-                        deleteSections:(NSIndexSet *)deleteSections
-                          moveSections:(NSSet<IGListMoveIndex *> *)moveSections
-                      insertIndexPaths:(NSArray<NSIndexPath *> *)insertIndexPaths
-                      deleteIndexPaths:(NSArray<NSIndexPath *> *)deleteIndexPaths
-                      updateIndexPaths:(NSArray<NSIndexPath *> *)updateIndexPaths
-                        moveIndexPaths:(NSArray<IGListMoveIndexPath *> *)moveIndexPaths;
+                        moveIndexPaths:(NSArray<IGListMoveIndexPath *> *)moveIndexPaths NS_DESIGNATED_INITIALIZER;
 
 /**
  :nodoc:
