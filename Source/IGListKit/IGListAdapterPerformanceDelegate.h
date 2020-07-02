111a324f6d3ebbe1a35737943e3d4e8ad92f5ded
Nate Stedman <natesm@fb.com>
2020-07-01 17:40:11 -0700

    Fix import in IGListKit
    Summary: This header refers to `UICollectionViewCell` but does not import UIKit.
    
    Reviewed By: candance
    
    Differential Revision: D22340609
    
    fbshipit-source-id: 8d03a48d363ecf3b00b29f89828d83ab44615fd4

--- Source/IGListKit/IGListAdapterPerformanceDelegate.h
+++ Source/IGListKit/IGListAdapterPerformanceDelegate.h
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @class IGListAdapter;
 @class IGListSectionController;
