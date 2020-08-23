bfe344f969e7178d10aa82885dbadc1f921c5e5d
Yannick Spreen <yannick.spreen@gmail.com>
2020-08-23 11:24:05 +0200

    Update version.sh
    Reads the version with `PlistBuddy`. Taken from instagram/IGListKit#1423.

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,4 +6,4 @@
 
 cd "$(dirname "$(dirname "$0")")" || exit 1
 
-exec defaults read "$(pwd)/Source/Info" CFBundleShortVersionString
+/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "$(pwd)/Source/Info.plist"
