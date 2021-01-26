64d9bc570b8ad468b77460fdace4d1f02985216a
Nate Stedman <natesm@fb.com>
2021-01-26 07:32:41 -0800

    Add refined Swift wrapper for IGListSingleSectionController
    Summary: This API is dependent on dynamic types right now, making it awkward and unsafe to use in Swift. By providing a generic wrapper, we can make it safer to use.
    
    Reviewed By: joetam
    
    Differential Revision: D26057302
    
    fbshipit-source-id: ec5d5ed202900f6171761214900fde1f9615e7f5

--- Examples/Examples-iOS/IGListKitExamples/Views/SpinnerCell.swift
+++ Examples/Examples-iOS/IGListKitExamples/Views/SpinnerCell.swift
@@ -6,22 +6,20 @@
  */
 
 import IGListKit
+import IGListSwiftKit
 import UIKit
 
 func spinnerSectionController() -> ListSingleSectionController {
-    let configureBlock = { (item: Any, cell: UICollectionViewCell) in
-        guard let cell = cell as? SpinnerCell else { return }
+    let configureBlock = { (_: Any, cell: SpinnerCell) in
         cell.activityIndicator.startAnimating()
     }
 
-    let sizeBlock = { (item: Any, context: ListCollectionContext?) -> CGSize in
+    let sizeBlock = { (_: Any, context: ListCollectionContext?) -> CGSize in
         guard let context = context else { return .zero }
         return CGSize(width: context.containerSize.width, height: 100)
     }
 
-    return ListSingleSectionController(cellClass: SpinnerCell.self,
-                                       configureBlock: configureBlock,
-                                       sizeBlock: sizeBlock)
+    return ListSingleSectionController(configure: configureBlock, size: sizeBlock)
 }
 
 final class SpinnerCell: UICollectionViewCell {
