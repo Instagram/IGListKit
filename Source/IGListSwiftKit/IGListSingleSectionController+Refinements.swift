aba955f1acd242217cecd0ccf6db764f6c2c1c8e
Erich Graham <erichg@fb.com>
2021-03-12 10:21:49 -0800

    Add Swift value type initializer override for IGListSingleSectionController
    Summary: Adds an init override if the generic type is a Swift value conforming to `ListIdentifiable`.
    
    Reviewed By: natestedman
    
    Differential Revision: D27013270
    
    fbshipit-source-id: 44f5c45e6396643d251c23bfedb6cd0a482e3913

--- Source/IGListSwiftKit/IGListSingleSectionController+Refinements.swift
+++ Source/IGListSwiftKit/IGListSingleSectionController+Refinements.swift
@@ -30,4 +30,37 @@ extension ListSingleSectionController {
             sizeBlock: { size($0 as! Item, $1) }
         )
     }
+
+    /**
+     Creates a new section controller for a given cell type that will always have only one cell when present in a list. Supports any Swift value conforming to `ListIdentifiable`.
+
+     - Parameters:
+        - configure: A closure that configures the cell with the Swift value item given to the section controller.
+        - size: A closure that returns the size for the cell given the collection context.
+
+     - Returns: A new section controller.
+
+     @warning Be VERY CAREFUL not to create retain cycles by holding strong references to: the object that owns the adapter
+     (usually `self`) or the `ListAdapter`. Pass in locally scoped objects or use `weak` references!
+    */
+    public convenience init<Value: ListIdentifiable, Cell: UICollectionViewCell>(
+        configure: @escaping (Value, Cell) -> Void,
+        size: @escaping (Value, ListCollectionContext?) -> CGSize
+    ) {
+        self.init(
+            cellClass: Cell.self,
+            configureBlock: { (item: Any, cell: UICollectionViewCell) in
+                guard let value = Value(diffable: item) else {
+                    fatalError("Expected object for value section controller to be a boxed \(Value.self), but it was \(item)")
+                }
+                configure(value, cell as! Cell)
+            },
+            sizeBlock: { (item: Any, context: ListCollectionContext?) in
+                guard let value = Value(diffable: item) else {
+                    fatalError("Expected object for value section controller to be a boxed \(Value.self), but it was \(item)")
+                }
+                return size(value, context)
+            }
+        )
+    }
 }
