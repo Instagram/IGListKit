b82266e5da5a0fdb25fe1e0f5a4f8e28ae3dc404
Andrey Yastrebov <Andrey.Yastrebov@jetbrains.com>
2021-05-26 10:41:49 +0300

    fix build errors & remove obsolete files

--- Examples/Examples-iOS/Pods/Target Support Files/Pods-IGListKitTodayExample/Pods-IGListKitTodayExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IGListKitTodayExample : NSObject
-@end
-@implementation PodsDummy_Pods_IGListKitTodayExample
-@end
--- Source/IGListDiffKit/IGListBatchUpdateData.mm
+++ Source/IGListDiffKit/IGListBatchUpdateData.mm
@@ -7,7 +7,11 @@
 
 #import "IGListBatchUpdateData.h"
 
+#if TARGET_OS_EMBEDDED || TARGET_OS_SIMULATOR
 #import <UIKit/UIKit.h>
+#else
+#import <Cocoa/Cocoa.h>
+#endif
 
 #import <unordered_map>
 
