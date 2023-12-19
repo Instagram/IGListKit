7fe78a887df91421550d13d38ccc10d54bc3c466
Maxime Ollivier <maxo@meta.com>
2023-12-19 07:17:30 -0800

    add grid to UICollectionViewCompositionalLayout example
    Summary: Adding a grid
    
    Differential Revision: D52260530
    
    fbshipit-source-id: 82e0ef4c1b0cad4a95e0c802d783d7c3f44c5f3b

--- Examples/Examples-iOS/IGListKitExamples/SectionControllers/GridSectionController.swift
+++ Examples/Examples-iOS/IGListKitExamples/SectionControllers/GridSectionController.swift
@@ -9,41 +9,6 @@ import IGListKit
 import IGListSwiftKit
 import UIKit
 
-final class GridItem: NSObject {
-
-    let color: UIColor
-    let itemCount: Int
-
-    var items: [String] = []
-
-    init(color: UIColor, itemCount: Int) {
-        self.color = color
-        self.itemCount = itemCount
-
-        super.init()
-
-        self.items = computeItems()
-    }
-
-    private func computeItems() -> [String] {
-        return [Int](1...itemCount).map {
-            String(describing: $0)
-        }
-    }
-}
-
-extension GridItem: ListDiffable {
-
-    func diffIdentifier() -> NSObjectProtocol {
-        return self
-    }
-
-    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
-        return self === object ? true : self.isEqual(object)
-    }
-
-}
-
 final class GridSectionController: ListSectionController {
 
     private var object: GridItem?
