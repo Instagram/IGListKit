832c770cd95aeb4d7dc28cabaec30f67ead864c6
Maxime Ollivier <maxo@meta.com>
2024-01-05 10:42:03 -0800

    add more details to the error message when catching NSInternalInconsistencyException
    Summary: We want to remove this exception handling, but we're seeing quite a lot of `IGFailure` happening recently. Lets improve the error message to make it easier to debug. Once we get down to a reasonable number, we can remove it.
    
    Reviewed By: fabiomassimo
    
    Differential Revision: D52564499
    
    fbshipit-source-id: f70ca2f0f0f8124377eeb2a219878dfa019af87c

--- Source/IGListKit/Internal/IGListBatchUpdateTransaction.m
+++ Source/IGListKit/Internal/IGListBatchUpdateTransaction.m
@@ -234,7 +234,7 @@ willPerformBatchUpdatesWithCollectionView:self.collectionView
             /// with our product team to properly fix their data source changes outside of the -performBatchUpdatesBlock:
             /// IGLisKit processed a new being as an assert that requires investigation,
             /// since it will be processed as invalid data source state that needs a reload.
-            IGFailure(@"The data source returned an invalid number of sections and rows due to updates applied outside of the performBatchUpdates block. This will cause a crash.");
+            IGFailure(@"IGListKit caught exception (%@): %@", exception.name, exception.reason);
             [self begin];
             return;
         } else {
