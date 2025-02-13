087f81ec6a7ff2cf505dcf251ee8b9ce5a013eab
Hoa Dinh <dvh@meta.com>
2025-02-13 09:36:58 -0800

    Added assert when returned -sectionControllerForSection is nil
    Summary: When the value is nil, it will result in a crash on iOS 18.
    
    Differential Revision: D69565468
    
    fbshipit-source-id: 5936ec253cd1e0dfb8b494ebb1364c6a14e79170

--- Source/IGListKit/Internal/IGListAdapter+UICollectionView.m
+++ Source/IGListKit/Internal/IGListAdapter+UICollectionView.m
@@ -43,13 +43,14 @@
     [performanceDelegate listAdapterWillCallDequeueCell:self];
 
     IGListSectionController *sectionController = [self sectionControllerForSection:indexPath.section];
+    IGAssert(sectionController != nil, @"Section controller nil for section %d", (int)indexPath.section);
 
 #if IG_ASSERTIONS_ENABLED
     if (!_dequeuedCells) {
         _dequeuedCells = [NSMutableSet new];
     }
 #endif
-    
+
     // flag that a cell is being dequeued in case it tries to access a cell in the process
     _isDequeuingCell = YES;
     UICollectionViewCell *cell = [sectionController cellForItemAtIndex:indexPath.item];
@@ -58,7 +59,7 @@
     IGAssert(cell != nil, @"Returned a nil cell at indexPath <%@> from section controller: <%@>", indexPath, sectionController);
     if (cell) {
         IGAssert(cell.reuseIdentifier != nil, @"Returned a cell without a reuseIdentifier at indexPath <%@> from section controller: <%@>", indexPath, sectionController);
-        
+
         if (_dequeuedCells) {
             // This will cause a crash in iOS 18
             IGAssert([_dequeuedCells containsObject:cell], @"Returned a cell (%@) that was not dequeued at indexPath %@ from section controller %@", NSStringFromClass([cell class]), indexPath, sectionController);
@@ -77,7 +78,7 @@
 - (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
     IGListSectionController *sectionController = [self sectionControllerForSection:indexPath.section];
     id <IGListSupplementaryViewSource> supplementarySource = [sectionController supplementaryViewSource];
-    
+
 #if IG_ASSERTIONS_ENABLED
     if (!_dequeuedSupplementaryViews) {
         _dequeuedSupplementaryViews = [NSMutableSet new];
@@ -90,7 +91,7 @@
     _isDequeuingSupplementaryView = NO;
 
     IGAssert(view != nil, @"Returned a nil supplementary view at indexPath <%@> from section controller: <%@>, supplementary source: <%@>", indexPath, sectionController, supplementarySource);
-    
+
     if (view && _dequeuedSupplementaryViews) {
         // This will cause a crash in iOS 18
         IGAssert([_dequeuedSupplementaryViews containsObject:view], @"Returned a supplementary-view (%@) that was not dequeued at indexPath %@ from supplementary source %@", NSStringFromClass([view class]), indexPath, supplementarySource);
