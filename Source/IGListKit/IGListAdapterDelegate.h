fa50aa1cf1be85940787e211d23eb5e7873dbf24
Jesse Seidman <jseidman@meta.com>
2025-04-24 13:07:16 -0700

    Add IGListAdapterDelegate Methods 1/n
    Summary:
    ## CONTEXT
    `IGListAdapterDelegate` currently has methods that are fired when an object is *first* displayed and when an object *ended* display on screen.  For many use cases this works but there are use cases in which it would be helpful to know whenever a cell will be displayed or ends display.  This is especially relevant in the `IGListAdapterDelegateAnnouncer` which will enable you to add code for tracking cells globally.  This change also balances the `IGListAdapterDelegate` with `IGListDisplayDelegate`  which has methods which include the cell as well
    
    ## PLAN
    This diff will be go out in multiple parts
    - delegate method addition (many file change due to our pattern of not preferring default implementation of protocol methods)
    - Update to `IGListDisplayHandler` and `IGListAdapterDelegateAnnouncer` for invocation of delegate methods
    - Unit test additions
    
    ## THIS DIFF
    This diff changes the delegate to include the methods that pass in the cell.  This is a huge many file diff because we prefer to not include default implementations of protocol methods.  This makes the delegate change a breaking change.
    
    Reviewed By: maxolls
    
    Differential Revision: D70348102
    
    fbshipit-source-id: 40709ba8a9112d8855d5ce5ecfd5930b5c79bf26

--- Source/IGListKit/IGListAdapterDelegate.h
+++ Source/IGListKit/IGListAdapterDelegate.h
@@ -6,6 +6,7 @@
  */
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @class IGListAdapter;
 
@@ -35,6 +36,34 @@ NS_SWIFT_NAME(ListAdapterDelegate)
  @param index The index of the object in the list.
  */
 - (void)listAdapter:(IGListAdapter *)listAdapter didEndDisplayingObject:(id)object atIndex:(NSInteger)index;
+
+/**
+ Notifies the delegate that a list object is about to be displayed.  This method is distinct from willDisplayObject:atIndex
+ because this method gets called whenever a cell will be displayed on screen as opposed to willDisplayObject:atIndex
+ which only gets called for once per section.
+
+ @param listAdapter The list adapter sending this information.
+ @param object The object that will display.
+ @param cell The cell which contains the object that will display.
+ @param indexPath The index path of the object in the list.
+ */
+- (void)listAdapter:(IGListAdapter *)listAdapter willDisplayObject:(id)object
+               cell:(UICollectionViewCell *)cell
+            atIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ Notifies the delegate that a list object is no longer being displayed.  This method is distinct from didEndDisplayingObject:atIndex
+ because this method gets called whenever a cell ends display on screen as opposed to didEndDisplayingObject:atIndex
+ which only gets called once when the section fully ends display.
+
+ @param listAdapter The list adapter sending this information.
+ @param object The object that ended display.
+ @param cell The cell which contains the object that ended display.
+ @param indexPath The index path of the object in the list.
+ */
+- (void)listAdapter:(IGListAdapter *)listAdapter didEndDisplayingObject:(id)object
+               cell:(UICollectionViewCell *)cell
+            atIndexPath:(NSIndexPath *)indexPath;
 
 @end
 
