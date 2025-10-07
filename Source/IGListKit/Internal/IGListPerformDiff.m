e7ea4ae174c2d25d41ec880dac44f4a5e8ccfdf6
Tim Oliver <timoliver@meta.com>
2025-10-06 17:22:13 -0700

    Add Meta copyright to IGListPerformDiff.m
    Summary: One of the implementation files in IGListKit didn't have the appropriate copyright headers, which was causing one of the open source health checks to throw a warning.
    
    Reviewed By: DimaVartanian
    
    Differential Revision: D83947233
    
    fbshipit-source-id: c5ce059373a57b4b235810f42e21bb4ab5181051

--- Source/IGListKit/Internal/IGListPerformDiff.m
+++ Source/IGListKit/Internal/IGListPerformDiff.m
@@ -1,9 +1,9 @@
-//
-//  IGListDiffExecutor.m
-//  IGActionRowViewProvider
-//
-//  Created by Maxime Ollivier on 8/1/24.
-//
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #import "IGListPerformDiff.h"
 
