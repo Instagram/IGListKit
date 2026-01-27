7114273472755962e71b438950ec314cf3dd92f1
Tim Oliver <git@tim.dev>
2026-01-27 12:44:45 +0900

    Adds more test coverage to IGListBindingSectionControllerTests

--- Tests/IGListBindingSectionControllerTests.m
+++ Tests/IGListBindingSectionControllerTests.m
@@ -522,4 +522,20 @@
     [self waitForExpectationsWithTimeout:30 handler:nil];
 }
 
+- (void)test_whenSizeForItemAtIndex_withInvalidIndex_thatReturnsCGSizeZero {
+    [self setupWithObjects:@[
+                             [[IGTestDiffingObject alloc] initWithKey:@1 objects:@[@7, @"seven"]],
+                             ]];
+
+    IGTestDiffingSectionController *section = [self.adapter sectionControllerForObject:self.dataSource.objects.firstObject];
+
+    // Index out of bounds (too large)
+    CGSize sizeOutOfBounds = [section sizeForItemAtIndex:100];
+    XCTAssertTrue(CGSizeEqualToSize(sizeOutOfBounds, CGSizeZero));
+
+    // Negative index
+    CGSize sizeNegative = [section sizeForItemAtIndex:-1];
+    XCTAssertTrue(CGSizeEqualToSize(sizeNegative, CGSizeZero));
+}
+
 @end
