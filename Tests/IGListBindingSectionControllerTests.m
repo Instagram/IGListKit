c3abd12d056a2af1ad967a3b80390a726c0a2408
Tim Oliver <git@tim.dev>
2026-01-27 12:48:24 +0900

    Added unit test for covering nil states in context menu configuration

--- Tests/IGListBindingSectionControllerTests.m
+++ Tests/IGListBindingSectionControllerTests.m
@@ -538,4 +538,20 @@
     XCTAssertTrue(CGSizeEqualToSize(sizeNegative, CGSizeZero));
 }
 
+#if !TARGET_OS_TV
+- (void)test_whenContextMenuConfiguration_withNoSelectionDelegate_thatReturnsNil {
+    [self setupWithObjects:@[
+                             [[IGTestDiffingObject alloc] initWithKey:@1 objects:@[@7, @"seven"]],
+                             ]];
+
+    IGTestDiffingSectionController *section = [self.adapter sectionControllerForObject:self.dataSource.objects.firstObject];
+    section.selectionDelegate = nil;
+
+    if (@available(iOS 13.0, *)) {
+        UIContextMenuConfiguration *config = [section contextMenuConfigurationForItemAtIndex:0 point:CGPointZero];
+        XCTAssertNil(config);
+    }
+}
+#endif
+
 @end
